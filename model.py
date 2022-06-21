4e58aff324df19de454651c9f602d87639739271
RodrigoBuen <rbartecriativa@gmail.com>
2022-06-21 10:30:44 -0300

    API Crud

---
+++ model.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Boolean, Column, Integer, String
+from db_config import Base
+
+class Movies(Base):
+    __tablename__ = "movie"
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True, nullable=False)
+    movie_id = Column(String, unique=True, index=True, nullable=False)
+    movie_name = Column(String(255), index=True, nullable=False)
+    director = Column(String(100), index=True, nullable=False)
+    geners = Column(String, index=True, nullable=False)
+    membership_required = Column(Boolean, nullable=False, default=True)
+    cast = Column(String(255), index=True, nullable=False)
+    streaming_platform = Column(String, index=True)
